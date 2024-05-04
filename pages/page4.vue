4f0e0e8742dddd3651c5ebaab0221d97b81d2654
Tyler Ronning <applesandcarrots007@gmail.com>
2024-05-03 18:20:15 -0700

    Capitalization

--- pages/page4.vue
+++ pages/page4.vue
@@ -4,7 +4,7 @@
 <div class="flex flex-col md:flex-row text-3xl justify-center items-center font-semibold text-white comfortaa gap-40 py-5 px-5 translate-x-[-2.5%]">
   <a href="mailto:tomorrowcoding@gmail.com"><img src="public/Mail.png" alt="" class="max-h-[30vh]"></a>
   <a href="https://www.instagram.com/tomorrow_coding24/"><img src="public/Insta.png" alt="" class="max-h-[30vh]"></a>
-  <a href="https://twitter.com/tomorrowC24"><img src="public/twitter.webp" alt="" class="max-h-[20vh]"></a>
+  <a href="https://twitter.com/tomorrowC24"><img src="public/Twitter.webp" alt="" class="max-h-[20vh]"></a>
 </div>
 <br>
 <div class="text-3xl md:text-6xl text-center font-bold comfortaa text-emerald-500 py-5">OR</div>
